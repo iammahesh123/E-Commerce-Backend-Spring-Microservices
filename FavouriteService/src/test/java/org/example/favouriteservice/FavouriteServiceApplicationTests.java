737f998426efbbfd0869cc4e127354dcab290678
iammahesh123 <maheshkadambala18@gmail.com>
2024-04-01 23:25:15 +0530

    add the favourite service of the project

---
+++ FavouriteService/src/test/java/org/example/favouriteservice/FavouriteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.favouriteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavouriteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
