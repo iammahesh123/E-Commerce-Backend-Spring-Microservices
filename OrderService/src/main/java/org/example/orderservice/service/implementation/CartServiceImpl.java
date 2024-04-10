a288504b7f54ffcf05f8f2d676ab6d3cd90d1161
iammahesh123 <maheshkadambala18@gmail.com>
2024-04-10 22:31:11 +0530

    update the product service

--- OrderService/src/main/java/org/example/orderservice/service/implementation/CartServiceImpl.java
+++ OrderService/src/main/java/org/example/orderservice/service/implementation/CartServiceImpl.java
@@ -1,38 +1,89 @@
 package org.example.orderservice.service.implementation;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.orderservice.constant.AppConstant;
 import org.example.orderservice.dtos.CartDTO;
+import org.example.orderservice.dtos.UserDTO;
+import org.example.orderservice.exception.CartNotFoundException;
+import org.example.orderservice.helper.CartMapping;
+import org.example.orderservice.repository.CartRepository;
 import org.example.orderservice.service.CartService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@Transactional
+@Slf4j
+@RequiredArgsConstructor
 public class CartServiceImpl implements CartService {
+
+    private CartRepository cartRepository;
+    private final RestTemplate restTemplate;
+
     @Override
     public List<CartDTO> findAll() {
-        return null;
+
+        log.info("CartServiceImplementation, find all the CartDTOS");
+        return this.cartRepository.findAll()
+                .stream()
+                .map(CartMapping::map)
+                .map(cart -> {
+                    // Capture restTemplate in a local variable
+                    cart.setUserDTO(this.restTemplate.getForObject(
+                            AppConstant.DiscoveredDomainsApi.USER_SERVICE_API_URL + "/" + cart.getUserDTO().getUserId(),
+                            UserDTO.class));
+                    return cart;
+                })
+                .distinct()
+                .collect(Collectors.toUnmodifiableList());
     }
 
+
     @Override
-    public CartDTO findById(Integer orderId) {
-        return null;
+    public CartDTO findById(Integer cartId) {
+        log.info("CartServiceImplementation,Find the CartDTo by using cart Id");
+        return this.cartRepository.findById(cartId)
+                .map(CartMapping::map)
+                .map(cart -> {
+                    cart.setUserDTO(this.restTemplate.getForObject(AppConstant.DiscoveredDomainsApi
+                            .USER_SERVICE_API_URL + "/" + cart.getUserDTO().getUserId(), UserDTO.class));
+                    return cart;
+
+                })
+                .orElseThrow(() -> new CartNotFoundException(String.format("Cart with id: %d is not found", cartId)));
+
     }
 
     @Override
     public CartDTO save(CartDTO cartDTO) {
-        return null;
+        log.info("CartServiceImplementation, Save the cartDTOs");
+        return CartMapping.map(this.cartRepository
+                .save(CartMapping.map(cartDTO)));
     }
 
     @Override
     public CartDTO update(CartDTO cartDTO) {
-        return null;
+        log.info("CartServiceImplementation, Update the CartDTO");
+        return CartMapping.map(this.cartRepository
+                .save(CartMapping.map(cartDTO)));
     }
 
     @Override
     public CartDTO update(Integer cartId, CartDTO cartDTO) {
-        return null;
+        log.info("CartServiceImplementation, Update the cartDTO using cartID");
+        return CartMapping.map(this.cartRepository
+                .save(CartMapping.map(findById(cartId))));
     }
 
     @Override
     public void deleteById(Integer cartId) {
+        log.info("CartServiceImplementaion, Delete the cartDTO using cartId");
+        this.cartRepository.deleteById(cartId);
 
     }
 }
--- OrderService/src/main/java/org/example/orderservice/service/implementation/OrderServiceImpl.java
+++ OrderService/src/main/java/org/example/orderservice/service/implementation/OrderServiceImpl.java
@@ -1,11 +1,28 @@
 package org.example.orderservice.service.implementation;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.orderservice.dtos.OrderDTO;
+import org.example.orderservice.repository.CartRepository;
+import org.example.orderservice.repository.OrderRepository;
 import org.example.orderservice.service.OrderService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+
+@Service
+@Transactional
+@Slf4j
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+
+    private OrderRepository orderRepository;
+    private RestTemplate restTemplate;
+
+
     @Override
     public List<OrderDTO> findAll() {
         return null;
